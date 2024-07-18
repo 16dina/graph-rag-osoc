cb576284a15fb3d84dac85e744f4c24e14ce2ac3
Dina Boshnaq <r0823887@student.thomasmore.be>
2024-07-18 15:19:10 +0200

    ...

--- app.py
+++ app.py
@@ -187,6 +187,8 @@ if prompt := st.chat_input():
     Answer in the language the user asked in. Be sure to be friendly and explain in a way an ordinary person can understand. The language city officials use is often very different from the language citizens use (officials wind up speaking in departments and form numbers instead of needs in big organizations).
 
     Show the {resources} to the end-user so they can refer to them. Format the links where the {resources_names} are shown as links being {resources}.
+
+    If you don't have any answer or potential resources from the decisions which are results of the query, don't refer to any external links (including the city of Gent's website).
     """
 
     completion_2 = client.chat.completions.create(
